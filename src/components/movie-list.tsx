5b227e1483b701a7e71015185b0bd360700d4ce2
A.A.A <51359436+SpUm09@users.noreply.github.com>
2020-01-26 15:56:17 +0700

    Update movie-list.tsx

--- src/components/movie-list.tsx
+++ src/components/movie-list.tsx
@@ -4,7 +4,7 @@ import { IMovieItem, MovieItem } from '../components/movie-item';
 export const MoviesList = (props: IMovieItem[]) => {
     return (
         <section className='movie-list'>
-            {Object.keys(props).map((_, index) => {
+            {Object.keys(props).map((item, index) => {
                 return <MovieItem data={{ ...props[index] }} />;
             })}
         </section>
